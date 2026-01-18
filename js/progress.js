403d32526d662277797aee079bc8a4490b9b5cb3
Nek1207 <Nek1207@yandex.ru>
2026-01-18 22:07:34 +0500

    ver202601181

--- js/progress.js
+++ js/progress.js
@@ -13,5 +13,5 @@ const interval = setInterval(() => {
   progress.value = value;
   percent = (progress.value / progress.max) * 100;
   value += 1;
-  if (value >= 543+2) clearInterval(interval);
+  if (value >= 577+2) clearInterval(interval);
 }, 1);
